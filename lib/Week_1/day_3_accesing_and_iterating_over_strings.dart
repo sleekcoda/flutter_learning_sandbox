b5b052882430b3d9e7a249602e2fa6b3dea327cd
Shalom <anthonyadahshalom@gmail.com>
2025-11-19 10:53:02 +0100

    add day_5_introduction_to_data_types.dart

--- lib/Week_1/day_3_accesing_and_iterating_over_strings.dart
+++ lib/Week_1/day_3_accesing_and_iterating_over_strings.dart
@@ -2,7 +2,7 @@ import 'day_2_string_properties_Basic_Methods_and_logical_operators.dart';
 void main() {
   String text = 'Hello';
   List<int> asciiValues = text.codeUnits;
-  print(asciiValues);
+  print(asciiValues); 
 
   String letter = 'A';
   int asciiValue = letter.codeUnitAt(0); // Get ASCII value of 'A'
