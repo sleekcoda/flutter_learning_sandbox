12d3b6f3eed9a0b73eecb8a09842150da3485045
Shalom <anthonyadahshalom@gmail.com>
2025-11-25 12:25:33 +0100

    add day_5_test_folder_for_data_types.dart

---
+++ lib/Week_1/day_5_test_folder_for_data_types.dart
@@ -0,0 +1,10 @@
+void main() {
+List<String> gfg = List<String>.filled(3, "default");
+gfg[0] = "Geeks";
+gfg[1] = "for";
+gfg[2] = "Geeks";
+print(gfg);
+print(gfg[0]);
+Set<String> countries = {"Germany", "NewZealand", "India", "UK"}; 
+print(countries);
+}
